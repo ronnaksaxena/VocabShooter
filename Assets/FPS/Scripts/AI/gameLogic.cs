a1550a2c9cba52f63c84b10712781b5e75893b01
ronnaksaxena <rsaxena@hamilton.edu>
2022-04-08 10:59:25 +0100

    initial commit

---
+++ Assets/FPS/Scripts/AI/gameLogic.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TextMeshProUGUI; //package not showing up, need assembly reference?
+//https://stackoverflow.com/questions/61102508/tmpro-not-found-in-visual-studio-code
+
+using TMPro;
+
+public class gameLogic : MonoBehaviour
+{
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public GameObject Boss;
+    public GameObject e1Text;
+    public GameObject e2Text;
+    public TextMeshProUGUI e3Text;
+    public GameObject BossText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy2 == null)
+            e3Text.text = "Gargantuan";
+        {
+        }
+
+    }
+}
